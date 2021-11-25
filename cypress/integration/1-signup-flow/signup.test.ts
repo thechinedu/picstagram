80897a4b7862a0b9f03ca05f11e931f0aa35e459
Chinedu Daniel <chinedudaniel7@gmail.com>
2021-11-25 08:35:56 +0100

    Add in functionality to create/destroy user accounts used in e2e test

--- cypress/integration/1-signup-flow/signup.test.ts
+++ cypress/integration/1-signup-flow/signup.test.ts
@@ -1,14 +1,46 @@
 /**
- * create cypress test user by making a request to firebase auth rest endpoint (use cypress.request)
+ * DONE create cypress test user by making a request to firebase auth rest endpoint (use cypress.request)
  
- * Test that user sign up is allowed via UI
+ * DONE Test that user sign up is allowed via UI
  * Test that the right hint shows up
- * Delete test user after test is complete
+ * DONE Delete test user after test is complete
  * Test that user signup is not allowed if email is already in use
  * 
  */
 
+let idToken: string;
+const identityURL = Cypress.env("FIREBASE_AUTH_EMULATOR_IDENTITY_URL");
+const accountsURL = Cypress.env("FIREBASE_AUTH_EMULATOR_ACCOUNTS_URL");
+const apiKey = Cypress.env("FIREBASE_API_KEY");
+
+const createCypressUser = () => {
+  cy.request({
+    method: "POST",
+    url: `${identityURL}/accounts:signUp?key=${apiKey}`,
+    body: {
+      email: "cypress-user@example.com",
+      password: "s3cr3tp@ss!",
+    },
+  }).then((response) => {
+    idToken = response.body.idToken;
+    expect(response.status).to.equal(200);
+  });
+};
+
+const clearUserAccounts = () => {
+  cy.request({
+    method: "DELETE",
+    url: accountsURL,
+  }).then((response) => {
+    expect(response.status).to.equal(200);
+  });
+};
+
 describe("Signup flow", () => {
+  afterEach(() => {
+    clearUserAccounts();
+  });
+
   context("When valid values are provided", () => {
     it("allows user signup", () => {
       cy.visit("/sign-up");
@@ -26,6 +58,10 @@ describe("Signup flow", () => {
 
       // TODO: this assertion will change and is temporary until the actual dashboard for logged in users is implemented
       cy.contains("Homepage for signed in user");
+    });
+
+    it("basic test", () => {
+      cy.visit("/sign-up");
     });
   });
 });
