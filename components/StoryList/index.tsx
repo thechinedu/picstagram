79a428b9c6bc0aeb54d019597368a956053bed1c
Chinedu Daniel <chinedudaniel7@gmail.com>
2021-12-17 07:32:20 +0100

    Start scroll handler implementation for StoryList componnent

--- components/StoryList/index.tsx
+++ components/StoryList/index.tsx
@@ -3,19 +3,36 @@ import styles from "./index.module.css";
 import Box from "@components/Box";
 import Story from "@components/Story";
 import { CircleLeft, CircleRight } from "@components/Icons";
-import { FC } from "react";
+import { FC, useRef } from "react";
 
-// Formula for calculating the amount of pages for the stories
-// total_items / (scroll_left_width / size of individual item)  => Math.round(10/ (425/425))
-
-const ScrollOffset = 320;
+const SCROLL_OFFSET = 375;
 
 const StoryList: FC = () => {
-  const handleScrollToRight = () => {};
+  const mainContainerRef = useRef<HTMLDivElement>(null);
+
+  const handleScrollToRight = () => {
+    const container = mainContainerRef.current as HTMLDivElement;
+    const currentWidth = container.getBoundingClientRect().width;
+    const { scrollWidth } = container;
+    const pageTotal = Math.ceil(scrollWidth / currentWidth);
+
+    container.scrollBy({
+      left: SCROLL_OFFSET,
+      behavior: "smooth",
+    });
+
+    setTimeout(() => {
+      console.log({
+        scrollLeft: container.scrollLeft,
+        pageTotal,
+        currentWidth,
+      });
+    }, 1);
+  };
 
   return (
     <Box className={styles.mainContainer}>
-      <div className={styles.storyContainer}>
+      <div className={styles.storyContainer} ref={mainContainerRef}>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
           <Story key={i} />
         ))}
