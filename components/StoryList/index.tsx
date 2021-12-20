a2cac52849167394542be022918a471f19fb7d1e
Chinedu Daniel <chinedudaniel7@gmail.com>
2021-12-20 07:30:08 +0100

    Finish scroll handler implementation for StoryList component

--- components/StoryList/index.tsx
+++ components/StoryList/index.tsx
@@ -3,42 +3,62 @@ import styles from "./index.module.css";
 import Box from "@components/Box";
 import Story from "@components/Story";
 import { CircleLeft, CircleRight } from "@components/Icons";
-import { FC, useRef } from "react";
+import { FC, useRef, useState } from "react";
 
 const SCROLL_OFFSET = 375;
 
 const StoryList: FC = () => {
   const mainContainerRef = useRef<HTMLDivElement>(null);
+  const [isFirstPage, setIsFirstPage] = useState(true);
+  const [isLastPage, setIsLastPage] = useState(false);
 
-  const handleScrollToRight = () => {
+  const handleScrollClick = (dir: "left" | "right") => () => {
     const container = mainContainerRef.current as HTMLDivElement;
-    const currentWidth = container.getBoundingClientRect().width;
-    const { scrollWidth } = container;
-    const pageTotal = Math.ceil(scrollWidth / currentWidth);
+    const left = dir === "left" ? -SCROLL_OFFSET : SCROLL_OFFSET;
 
     container.scrollBy({
-      left: SCROLL_OFFSET,
+      left,
       behavior: "smooth",
     });
+  };
 
-    setTimeout(() => {
-      console.log({
-        scrollLeft: container.scrollLeft,
-        pageTotal,
-        currentWidth,
-      });
-    }, 1);
+  const handleScroll = () => {
+    const container = mainContainerRef.current as HTMLDivElement;
+    const { scrollWidth, scrollLeft } = container;
+
+    if (scrollWidth - scrollLeft <= SCROLL_OFFSET) setIsLastPage(true);
+    else if (scrollLeft >= 10) {
+      setIsFirstPage(false);
+      setIsLastPage(false);
+    } else {
+      setIsFirstPage(true);
+      setIsLastPage(false);
+    }
   };
 
   return (
     <Box className={styles.mainContainer}>
-      <div className={styles.storyContainer} ref={mainContainerRef}>
+      <div
+        className={styles.storyContainer}
+        ref={mainContainerRef}
+        onScroll={handleScroll}
+      >
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
           <Story key={i} />
         ))}
       </div>
-      <CircleLeft className={styles.iconLeft} />
-      <CircleRight className={styles.iconRight} onClick={handleScrollToRight} />
+      {!isFirstPage && (
+        <CircleLeft
+          className={styles.iconLeft}
+          onClick={handleScrollClick("left")}
+        />
+      )}
+      {!isLastPage && (
+        <CircleRight
+          className={styles.iconRight}
+          onClick={handleScrollClick("right")}
+        />
+      )}
     </Box>
   );
 };
